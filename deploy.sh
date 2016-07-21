d655c522845610cbb2c07ca0c6922575a41c9b8e
Kumar Ayush <cheekujodhpur@gmail.com>
2016-07-21 08:18:56 +0530

    fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-jekyll build
-=======
-jekyll clean
-jekyll build --incremental --trace
->>>>>>> cff62a88a421bf36cd5f1c9ffd9f04919ebc001c
-sudo cp -r _site/* /var/www/html/
+jekyll serve --incremental --trace
