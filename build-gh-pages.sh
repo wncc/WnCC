9e78376140f4319787202f112bad36b9abc8d675
Varun Patil <radialapps@gmail.com>
2018-04-09 22:04:51 +0530

    Add script

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -3,3 +3,18 @@
 mkdir built
 gem install jekyll
 JEKYLL_ENV=production jekyll build --destination built --config _config.gh-pages.yml
+
+git config --global user.name "Xunk-Bot"
+git config --global user.email "xunk@radialapps.com"
+git config --global push.default matching
+git clone git@github.com:pulsejet/wncc-all-deploy.git deploy
+
+cd deploy
+rm -r *
+mv -R ../built/* ./
+date > BUILD_TIME
+git add -A
+git commit -m "Automated Build"
+git tag $TRAVIS_COMMIT
+git push
+cd ..
