a71b98054ba688f446b64ff5c6ccdb2fe4230c0e
MayuBhattu <37226641+MayuBhattu@users.noreply.github.com>
2018-05-01 22:49:59 +0530

    Added NLP and rumour projects (#190)
    * Create rumour.md
    
    * rumour kushal babel
    
    rumour image
    
    * project name corrected
    
    * Update rumour.md
    
    * Update 14-kushal- rumour.md
    
    * Delete rumour.md
    
    * Create 24-aviral-siddhant-semantics.md
    
    * Update 24-aviral-siddhant-semantics.md
    
    * Update 24-aviral-siddhant-semantics.md
    
    * Update 24-aviral-siddhant-semantics.md
    
    * Images added
    
    * Create 20-ritwick-shubham-mri-ct.md
    
    * Update 20-ritwick-shubham-mri-ct.md
    
    * Update 20-ritwick-shubham-mri-ct.md
    
    * fixed resize.py
    
    * build

--- _site/images/resize.py
+++ _site/images/resize.py
@@ -3,7 +3,7 @@ import glob
 
 images = glob.glob("*")
 print images
-ignored = ['banner.jpg', 'coding.jpg', 'pic01.jpg', 'pic02.jpg', 'pic03.jpg', 'seasons.jpg', 'soc.jpg', 'resize.py','HoldTheBeat.png','JARVIS.jpg','404.png']
+ignored = ['banner.jpg', 'coding.jpg', 'pic01.jpg', 'pic02.jpg', 'pic03.jpg', 'seasons.jpg', 'soc.jpg', 'resize.py','HoldTheBeat.png','JARVIS.jpg','404.png','collaborations','team','initiatives','yahoohacku-eventCover.jpg']
 
 for image in images:
     if image not in ignored:
